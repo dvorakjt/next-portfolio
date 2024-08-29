afb87d0528fb91b2a5503e6e7e3c76d7edfc4326
Joseph Dvorak <dvorakjt@gmail.com>
2024-08-29 15:54:25 -0400

    added favicon, imported fonts through next/font/google, updated hero text

--- src/components/home/animated-text/animated-text.tsx
+++ src/components/home/animated-text/animated-text.tsx
@@ -8,7 +8,7 @@ import styles from "./styles.module.css";
 
 const text = [
   "Hello, World!",
-  "I'm Joe, a software engineer.",
+  "I'm Joe, a developer.",
   "Welcome to my portfolio!",
 ];
 
---
+++ src/fonts/z-cool.ts
@@ -0,0 +1,7 @@
+import { ZCOOL_KuaiLe } from "next/font/google";
+
+export const zCool = ZCOOL_KuaiLe({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-family-z-cool",
+});
